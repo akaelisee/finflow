4db6755f2635fb81269d1413de0b6e84e40407f3
Missa Constant <constant.baih@operantic.com>
2026-09-05 20:13:59 +0200

    update

--- apps/web/src/api/categorys.ts
+++ apps/web/src/api/categorys.ts
@@ -17,3 +17,15 @@ export async function createCategories(data: CategoriesPlayload): Promise<Catego
     const response = await axios.post(API, data);
     return response.data;
 }
+
+export async function updateCategories(id: string, data: CategoriesPlayload): Promise<Categories> {
+    console.log("ID envoyé :", id);
+    console.log("URL appelée :", `${API}/${id}`);
+    console.log("DATA :", data);
+    const response = await axios.patch(`${API}/${id}`, data );
+    return response.data;
+}
+
+export async function deleteCategories(id: string): Promise<void> {
+    await axios.delete(`${API}/${id}`);
+}
